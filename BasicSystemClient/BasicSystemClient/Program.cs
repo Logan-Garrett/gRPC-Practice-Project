ca6b532b802c54ec08f80848cbeb18784e1ada73
Logan <ninetailsoftware@gmail.com>
2024-07-30 22:05:01 -0400

    Getting Ready for Big Project.

---
+++ BasicSystemClient/BasicSystemClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+// using GrpcService;
+using BasicSystemClient;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        // The port number must match the port of the gRPC server.
+        using var channel = GrpcChannel.ForAddress("https://localhost:7128");
+        var client = new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(
+                          new HelloRequest { Name = "BasicSystemClient" });
+        Console.WriteLine("Greeting: " + reply.Message);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
